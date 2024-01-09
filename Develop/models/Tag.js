bb614a04431408e78039a5206f475b79d47f0fd7
Ali K <aliekx609@gmail.com>
2024-01-09 00:10:05 +0000

    addition of comments on tag.js

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,20 +1,25 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');                    // <= Imported necessary modules from Sequelize.
+const sequelize = require('../config/connection');                    // <= Imported the Sequelize connection from the configuration file.
 
-const sequelize = require('../config/connection.js');
+class Tag extends Model {}                                         
 
-class Tag extends Model {}
+Tag.init(                                                             // <= Defined and Initialized the Tag model with the specified attributes.
 
-Tag.init(
   {
-    // define columns
+    id: {                                                             // <= Defined the 'id' & 'tag_name' attributes with the specified properties.
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
     modelName: 'tag',
   }
 );
 
-module.exports = Tag;
+module.exports = Tag;                                               // <= Exported the Tag model for use in other parts of the application.
