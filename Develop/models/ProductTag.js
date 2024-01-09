b3e36084fa8921de5724750c7d6b10e4ffe9674e
Ali K <aliekx609@gmail.com>
2024-01-09 00:18:35 +0000

    addition of coments to productTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,35 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
+ProductTag.init(                                         // <= Defined & Initialized the ProductTag model with the specified attributes.
   {
-    // define columns
+    id: {                                                
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {                                      // <= Setted up a reference to the 'id' column in the 'product' model.
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {                                     // <= Setted up a reference to the 'id' column in the 'tag' model.
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+  { 
+    sequelize,                                         // <= Setted the Sequelized connection and the model name.
+    modelName: 'productTag',
   }
 );
 
-module.exports = ProductTag;
+module.exports = ProductTag;                          // <= Exported the ProductTag model for use in other parts of the application.
